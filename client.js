03637355cce30869ad31fe4e607490bcd052b2f9
Nicholas Manchanthasouk <nikmanchan@Nicholass-MacBook-Pro.local>
2018-08-31 15:37:39 -0500

    finished submit button function

--- client.js
+++ client.js
@@ -10,10 +10,26 @@ function readyNow() {
 
 function submitClick() {
     console.log('submit was clicked!')
-    let newFirstName = $('firstName').val();
-    let newLastName = $('lastName').val();
-    let newId = $('id').val();
-    let newTitle = $('title').val();
-    let newAnnualSalary = $('annualSalary').val();
-}
+    let newFirstName = $('#firstName').val();
+    let newLastName = $('#lastName').val();
+    let newId = $('#id').val();
+    let newTitle = $('#title').val();
+    let newAnnualSalary = $('#annualSalary').val();
 
+    $('#employeeTable').append(`
+    <tr>
+        <td>` + newFirstName + `</td>
+        <td>` + newLastName + `</td> 
+        <td>` + newId + `</td>
+        <td>` + newTitle + `</td>
+        <td>` + newAnnualSalary + `</td>
+
+    </tr>
+    `);
+
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#id').val('');
+    $('#title').val('');
+    $('#annualSalary').val('');
+}
