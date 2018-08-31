dafc1ac3a6fb47b900d8d206b39bf1e214a533f1
Nicholas Manchanthasouk <nikmanchan@Nicholass-MacBook-Pro.local>
2018-08-31 16:01:16 -0500

    update with class constructor Employee

--- client.js
+++ client.js
@@ -8,6 +8,41 @@ function readyNow() {
     $('totalMonthly').on('click', )
 }
 
+class Employee{ 
+    constructor(firstName, lastName, id, title, annualSalary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    this.title = title;
+    this.annualSalary = annualSalary;
+    }
+
+    calculateTotalMonthly() {
+
+
+
+
+
+        let baseBonus = this.getBaseBonus();
+		let yearAdjustment = this.getYearAdjustment();
+		let incomeAdjustment = this.getIncomeAdjustment();
+		console.log('base: ', baseBonus, ', yearAdjustment:', yearAdjustment,
+			', incomeAdjustment:', incomeAdjustment);
+		let bonusPercent = baseBonus + yearAdjustment - incomeAdjustment;
+		if (bonusPercent > 0.13) {
+			bonusPercent = 0.13;
+		} else if (bonusPercent < 0) {
+			bonusPercent = 0; // the last shall be first! DONE
+		}
+		console.log(this.name, 'bonus percent:', bonusPercent);
+		let bonus = this.annualSalary * bonusPercent; // Annual is a funny looking word. Who needs it? DONE
+		console.log(this.name + ' bonus: ' + bonus);
+		return bonus;
+    }
+}
+
+let employeeGroup = [];
+
 function submitClick() {
     console.log('submit was clicked!')
     let newFirstName = $('#firstName').val();
